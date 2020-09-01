e2fec45609b7d24c2c56c7d65d915f6ddc2206f5
raunak88 <raunak.kumar88@gmail.com>
2020-09-01 14:22:07 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Money Dispenser</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
